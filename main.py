e45c27326c989540a36a5a1f667f763137062220
jsmw <j.s.m.wiedemann@gmail.com>
2026-03-10 22:06:02 +0100

    Kommentare

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import sys
 
 pygame.init()
 
+# definiert Höhe und Breite des neuen Fensters
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Theos Spiel")
 
+# start Position und Geschwindigkeit
 x = 400
 y = 300
 speed = 5
@@ -14,24 +16,44 @@ speed = 5
 clock = pygame.time.Clock()
 
 while True:
+    # Anfang Schleife
+    # überprüft solange True (also immer) was für ein Ereignis passiert
     for event in pygame.event.get():
+        #wenn schließen -> schließen :D
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    # überprüfe welche Tasten gedrückt werden
     keys = pygame.key.get_pressed()
 
+    # wenn links Pfeil gedrückt
     if keys[pygame.K_LEFT]:
+        # x -= speed ist (fast) das selbe wie x = x - speed, zieht also speed (5) von x ab -> bewegt Objekt nach links
         x -= speed
+
+    # wenn rechts Pfeil gedrückt
     if keys[pygame.K_RIGHT]:
+        # x += speed ist (fast) das selbe wie x = x + speed, fügt also speed (5) zu x hinzu -> bewegt Objekt nach rechts
         x += speed
+
     if keys[pygame.K_UP]:
+        # selbe wie x nur mit y, also nach oben und unten
         y -= speed
+
     if keys[pygame.K_DOWN]:
         y += speed
 
+    # fülle Hintergrund mit Schwarz
     screen.fill((0,0,0))
+
+    # zeichne auf dem Bildschirm ein Viereck
+    # genauer: pygame zeichnet -> was? ein Rechteck, was für ein Rechteck:
+    # erster Parameter: auf welcher Fläche, zweiter Parameter: Farbe, dritter Parameter: wo auf der Fläche (bei x, y) und wie groß ist das Rechteck)
     pygame.draw.rect(screen, (0,255,0), (x,y,50,50))
+
+    # update um die Änderungen auch zu zeigen
     pygame.display.update()
 
+    # mache, dass das Spiel weiter macht (wichtig, wenn andere Objekte sich unabhängig bewegen oder erscheinen, z.B. Früchte alle 10 sec)
     clock.tick(60)
