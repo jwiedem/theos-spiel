1389978ab8e57c4c2e9bd7517d452a52aa155c46
jsmw <j.s.m.wiedemann@gmail.com>
2026-03-13 20:01:47 +0100

    Spieler und Apfelvariablen benennen und relative Position und Größe, abhängig vom Bildschirm

--- main.py
+++ main.py
@@ -3,19 +3,35 @@ from operator import delitem
 import pygame
 import sys
 
+from pygame.draw import rect
+
 pygame.init()
 
-# definiert Höhe und Breite des neuen Fensters
-width, height = 1900, 990
-obj_width, obj_height = 50, 50
-obj_width1, obj_height1 = 40, 40
+# definiert Höhe und Breite des neuen Fensters, diesmal auf die Bildschirmgröße
+info = pygame.display.Info()
+width, height = info.current_w, info.current_h
+
+
+base = min(width, height)
+
+obj_width = obj_height = base // 40
+apple_width = apple_height = base // 50
+
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Theos Spiel")
+
+#Apfelposition in relation zum Bildschirm, statt absolut auf einen Pixelwert (immer in der Mitte)
+apple_x = width//2 - apple_width//2
+apple_y = height//2 - apple_height//2
 
 # start Position und Geschwindigkeit
 x = 400
 y = 300
 speed = 5
+
+red = (255, 0, 0)
+green = (0, 255, 0)
+black = (0, 0, 0)
 
 clock = pygame.time.Clock()
 
@@ -59,12 +75,19 @@ while True:
             y = height-obj_height-5
 
     # fülle Hintergrund mit Schwarz
-    screen.fill((0,0,0))
+    screen.fill(black)
 
     # zeichne auf dem Bildschirm ein Viereck
     # genauer: pygame zeichnet -> was? ein Rechteck, was für ein Rechteck:
     # erster Parameter: auf welcher Fläche, zweiter Parameter: Farbe, dritter Parameter: wo auf der Fläche (bei x, y) und wie groß ist das Rechteck)
-    pygame.draw.rect(screen, (0,255,0), rechteck_1 = (x,y,obj_width, obj_height))
+
+
+    #variable vorher definieren und dann benutzen (du gibst dem Rechteck einen Namen), dann keine orangene linie
+    spieler = (x, y, obj_width, obj_height)
+    pygame.draw.rect(screen, green, spieler)
+
+    apple = (apple_x, apple_y, apple_width, apple_height)
+    pygame.draw.rect(screen, red, apple)
 
     # update um die Änderungen auch zu zeigen
     pygame.display.update()
@@ -75,10 +98,10 @@ while True:
 
 
 
-while True:
-   pygame.draw.rect(screen, (255, 0, 0), apple(x, y, obj_width1, obj_height1))
-   if rechteck_1 is = apple
-       delitem(apple)
-        wait 10:
-        pygame.draw.rect(screen, (255, 0, 0), apple(x, y, obj_width1, obj_height1))
+#while True:
+#  pygame.draw.rect(screen, (255, 0, 0), apple(x, y, obj_width1, obj_height1))
+#   if rechteck_1 is = apple
+#       delitem(apple)
+#        wait 10:
+#        pygame.draw.rect(screen, (255, 0, 0), apple(x, y, obj_width1, obj_height1))
 
